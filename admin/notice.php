0ea370e968cf3d04a7db1181656c944411df65fe
Tad <tad0616@gmail.com>
2019-05-01 11:37:48 +0800

    new \XoopsFormHiddenToken

--- admin/notice.php
+++ admin/notice.php
@@ -58,7 +58,7 @@ function tad_web_notice_form($NoticeID = '')
 
     //加入Token安全機制
     include_once XOOPS_ROOT_PATH . '/class/xoopsformloader.php';
-    $token = new XoopsFormHiddenToken();
+    $token = new \XoopsFormHiddenToken();
     $token_form = $token->render();
     $xoopsTpl->assign('token_form', $token_form);
     $xoopsTpl->assign('action', $_SERVER['PHP_SELF']);
